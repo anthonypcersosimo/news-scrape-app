5ad848efb76bdeb0f8c0a1f1dfdfb07688f28584
Anthony <anthony.cersosimo@gmail.com>
2019-07-31 20:06:30 -0400

    added a basic navbar to the pages and put an H1 on each page with the name of the page to distinguish the two pages, added a test object array in an API get route to display the info to the page. Will test out rendering HTML to the page with the info from the api route

--- routes/routes.js
+++ routes/routes.js
@@ -5,4 +5,24 @@ module.exports = (router) => {
     router.get("/saved", (req, res) => {
         res.render("saved");
     });
+
+    router.get("/api/stories", (req, res) => {
+        const stories = [{
+            headline: "headline 1",
+            author: "anthony",
+            sub: "This is a story about my life"
+        },
+        {
+            headline: "headline 2",
+            author: "anthony",
+            sub: "This is the story of my other life"
+        },
+        {
+            headline: "headline 3",
+            author: "anthony",
+            sub: "This is the story of my cat named muppets"
+        }
+        ];
+        res.json(stories);
+    });
 };
