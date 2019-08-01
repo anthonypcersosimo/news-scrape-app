bfffc20aa838225909a7b27e76fede7cbb31836e
Anthony <anthony.cersosimo@gmail.com>
2019-07-31 23:25:32 -0400

    added the controllers to be passed information and use the CRUD methods

--- controllers/headlines.js
+++ controllers/headlines.js
@@ -1,5 +1,39 @@
 const Headline = require("../models/headlines");
+const scrape = require("../scripts/scrape")
 
 module.exports = {
-    
+    fetch: function(cb) {
+        scrape(function(data) {
+            let articles = data;
+            articles.forEach(element => {
+                element.saved = false;
+            });
+
+            Headline.collection.insertMany(articles, {ordered: false}, function(err, data) {
+                cb(docs);
+            });
+        });
+    },
+    delete: function(query, cb) {
+        Headline.remove(query, cb);
+    },
+    get: function(query, cb) {
+        Headline.find(query)
+            .sort({
+                _id: -1
+            })
+            .exec(function(err, data) {
+                cb(data)
+            });
+    },
+    update: function(query, cb) {
+        Headline.update({
+            _id: query._id
+        },
+        {
+            $set: query
+        },
+        {},
+        cb);
+    }
 };
--- controllers/note.js
+++ controllers/note.js
@@ -1,5 +1,27 @@
 const Note = require("../models/note");
 
 module.exports = {
-    
+    get: function(data, cb) {
+        Note.find({
+            _headlineId: data._id
+        }, cb);
+    },
+    save: function(data, cb) {
+        let newNote = {
+            _headlineId: data._id,
+            date: data.date,
+            noteText: data.noteText
+        };
+        Note.create(newNote, (err, data) => {
+            if (err) throw err;
+            else {
+                cb(data);
+            }
+        });
+    },
+    delete: function(data, cb) {
+        Note.remove({
+            _id: data._id
+        }, cb);
+    }
 };
